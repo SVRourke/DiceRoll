ebeab35410b2de68e50dd17f6a3217c6f0c734af
SVRourke <samuel@rourke.tech>
2020-12-29 16:33:57 -0500

    added tts, partial stt

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var elements = document.getElementsByClassName("dice-block");
 var display_element = document.getElementById("rolled-value");
 
-var roll_dice = function() {
-    var attribute = this.getAttribute("data-sides");
-    update_rolled(get_random(attribute));
+const synth = window.speechSynthesis;
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const recognition = new window.SpeechRecognition();
+
+recognition.onresult = (e) {
+    const speechToText = e.results[0][0].transcript;
+
+}
+recognition.start();
+
+function roll_dice() {
+    var rolled = get_random(this.getAttribute("data-sides"))
+    update_rolled(rolled);
+    talk(rolled);
 }
 
 function get_random(max) {
@@ -15,8 +27,14 @@ function update_rolled(val) {
     display_element.textContent = val;
 }
 
+function talk(words) {
+    let utterance = new SpeechSynthesisUtterance("You rolled a " + words);
+    utterance.lang = "en-GB";
+    utterance.rate = 1;
+    synth.speak(utterance);
+    
+}
+
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', roll_dice, false)
 }
-
-
