7fffa3c2bae411d1e09367ee478abf41f9b39b55
SVR <samuelvanrourke@gmail.com>
2021-01-01 09:50:35 -0500

    refactored code

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 var elements = document.getElementsByClassName("dice-block");
 var display_element = document.getElementById("rolled-value");
 
-const synth = window.speechSynthesis;
-
-
 function roll_dice() {
+    var synth = window.speechSynthesis;
     var rolled = get_random(this.getAttribute("data-sides"))
     update_rolled(rolled);
-    talk(rolled);
+    talk(synth, rolled);
 }
 
 function get_random(max) {
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - 1) + 1);
+    return Math.floor(Math.random() * (max) + 1);
 }
 
 function update_rolled(val) {
     display_element.textContent = val;
 }
 
-function talk(words) {
-    var utterance = new SpeechSynthesisUtterance("You rolled a " + words);
-    utterance.lang = "en-GB";
+function talk(synth, words) {
+    var utterance = new SpeechSynthesisUtterance(words);
     utterance.rate = 1;
     synth.speak(utterance);
-    
 }
 
 for (var i = 0; i < elements.length; i++) {
