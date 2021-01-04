0b969c2506c2bbdc6ab7d6c06e115fe9f07599c8
SVRourke <samuel@rourke.tech>
2021-01-04 08:34:16 -0500

    refoactored / fixed random number function

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-var elements = document.getElementsByClassName("dice-block");
-var display_element = document.getElementById("rolled-value");
+const elements = document.getElementsByClassName("dice-block");
+const display_element = document.getElementById("rolled-value");
+const synth = window.speechSynthesis;
 
 function roll_dice() {
-    var synth = window.speechSynthesis;
-    var rolled = get_random(this.getAttribute("data-sides"))
+    let rolled = get_random(this.getAttribute("data-sides"))
     update_rolled(rolled);
     talk(synth, rolled);
 }
 
 function get_random(max) {
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max) + 1);
+    return Math.floor(Math.random() * (Math.floor(max) - 1) + 1);
 }
 
-function update_rolled(val) {
-    display_element.textContent = val;
-}
+function update_rolled(val) {display_element.textContent = val;}
 
 function talk(synth, words) {
-    var utterance = new SpeechSynthesisUtterance(words);
+    let utterance = new SpeechSynthesisUtterance(words);
     utterance.rate = 1;
     synth.speak(utterance);
 }
 
-for (var i = 0; i < elements.length; i++) {
+for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', roll_dice, false)
 }
