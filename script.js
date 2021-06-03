8b7d43aafa6e44ae9a06ae161afce6188f84ee25
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 09:04:50 -0400

    some refactoring, might just redo whole thing

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const elements = document.getElementsByClassName("dice-block");
-const display_element = document.getElementById("rolled-value");
-const synth = window.speechSynthesis;
+var display = document.getElementById("rolled-value");
+var synth = window.speechSynthesis;
 
 function roll_dice() {
-    let rolled = get_random(this.getAttribute("data-sides"))
-    update_rolled(rolled);
-    talk(synth, rolled);
+    let rolled = get_random(this.dataset['sides'])
+    display.textContent = rolled
+    talk(rolled);
 }
 
 function get_random(max) {
     return Math.floor(Math.random() * (Math.floor(max) - 1) + 1);
 }
 
-function update_rolled(val) {display_element.textContent = val;}
-
-function talk(synth, words) {
+function talk(words) {
     let utterance = new SpeechSynthesisUtterance(words);
     utterance.rate = 1;
     synth.speak(utterance);
 }
 
-for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', roll_dice, false)
+for (const element of elements) {
+    element.addEventListener('click', roll_dice, false)
 }
